fbca59c96dc079565a49be0ebf86d3560bd84655
Muhammad <inform.tariq@gmail.com>
2021-02-17 09:53:47 +0500

    Log error fixed. Commented Logs.go

--- logging/Logs.go
+++ logging/Logs.go
@@ -1,35 +1,35 @@
 package logging
 
-import (
-	"log"
-	"os"
-)
+// import (
+// 	"log"
+// 	"os"
+// )
 
-var (
-	warningLogger *log.Logger
-	infoLogger    *log.Logger
-	errorLogger   *log.Logger
-)
+// var (
+// 	warningLogger *log.Logger
+// 	infoLogger    *log.Logger
+// 	errorLogger   *log.Logger
+// )
 
-func init() {
-	file, err := os.OpenFile("/var/logs/exam105/exam105.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Println( "Problem with exam105.log ", err.Error())
-	}
+// func init() {
+// 	file, err := os.OpenFile("/var/logs/exam105/exam105.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+// 	if err != nil {
+// 		log.Println( "Problem with exam105.log ", err.Error())
+// 	}
 
-	infoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	warningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	errorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-}
+// 	infoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+// 	warningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+// 	errorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+// }
 
-func LogInformation(msg string) {
-	infoLogger.Println(msg)
-}
+// func LogInformation(msg string) {
+// 	infoLogger.Println(msg)
+// }
 
-func LogWarning(warningmsg string) {
-	warningLogger.Println(warningmsg)
-}
+// func LogWarning(warningmsg string) {
+// 	warningLogger.Println(warningmsg)
+// }
 
-func LogError(errorMsg string) {
-	errorLogger.Println(errorMsg)
-}
+// func LogError(errorMsg string) {
+// 	errorLogger.Println(errorMsg)
+// }
