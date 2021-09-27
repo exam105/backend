9d88a4911b5d049fdb3a3ddd643e0051dcb40fdd
Muhammad <inform.tariq@gmail.com>
2021-09-27 23:27:39 +0700

    is_theory and QuestionsIDs  parameters have been added to the Search Result.

--- domain/Search.go
+++ domain/Search.go
@@ -31,6 +31,8 @@ type SearchResult_Paper struct {
 	System  		string    			`json:"system,omitempty" bson:"system,omitempty"`
 	Board   		string    			`json:"board,omitempty" bson:"board,omitempty"`
 	Date    		time.Time 			`json:"date,omitempty" bson:"date"`
+	QuestionHexIds 	[]string           	`json:"question_hex_ids,omitempty" bson:"question_hex_ids"`
+	IsTheory       	bool               	`json:"is_theory,omitempty" bson:"is_theory"`
 }
 
 // Question Use Case / Service layer
