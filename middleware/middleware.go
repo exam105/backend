d541f34c0e4ffa4dac5d2da55b49ac9d4e163e4f
Muhammad <inform.tariq@gmail.com>
2021-11-22 14:37:48 +0700

    Access-Control-Allow-Methods and Access-Control-Allow-Headers allowed

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -12,6 +12,9 @@ func (m *GoMiddleware) CORS(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		c.Response().Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Response().Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        c.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 		return next(c)
 	}
 }
